3cf1e4b679974b44f87b3004e30a8dd0a53540e3
Edward <edward821220@gmail.com>
2024-04-19 19:47:56 +0800

    first commit

---
+++ server/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"graphql-workshop/graph"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func GinContextToContextMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := context.WithValue(c.Request.Context(), "GinContextKey", c)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
+
+func main() {
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		panic(envErr)
+	}
+
+	gin.SetMode(os.Getenv("GIN_MODE"))
+	r := gin.Default()
+
+	r.Use(GinContextToContextMiddleware())
+
+	r.POST("/query", graphqlHandler())
+
+	r.GET("/", playgroundHandler())
+
+	r.Run(":" + os.Getenv("PORT"))
+}
---
+++ server/tools/data.go
@@ -0,0 +1,92 @@
+package tools
+
+import "graphql-workshop/graph/model"
+
+var CompaniesData = []*model.Company{
+	{
+		ID:                "1",
+		Name:              "LINE BANK",
+		StockPrice:        180.24,
+		OutstandingShares: 16069000000,
+		Founded:           1975,
+		Headquarters:      "New York, NY",
+		Industry:          "Technology",
+		Employees: []*model.Employee{
+			{ID: "1", Name: "Riva", Updated: false},
+		},
+	},
+	{
+		ID:                "2",
+		Name:              "HTC",
+		StockPrice:        288.49,
+		OutstandingShares: 7524000000,
+		Employees: []*model.Employee{
+			{ID: "1", Name: "Claire", Updated: false},
+			{ID: "2", Name: "AFU", Updated: false},
+			{ID: "3", Name: "KK", Updated: false},
+		},
+		Founded:      1988,
+		Headquarters: "San Francisco, CA",
+		Industry:     "Retail",
+	},
+	{
+		ID:                "3",
+		Name:              "AppWorks School",
+		StockPrice:        3367.24,
+		OutstandingShares: 503000000,
+		Employees: []*model.Employee{
+			{ID: "1", Name: "Tzu Hua", Updated: false},
+		},
+		Founded:      1962,
+		Headquarters: "Los Angeles, CA",
+		Industry:     "Manufacturing",
+	},
+	{
+		ID:                "4",
+		Name:              "MEXC",
+		StockPrice:        2293.37,
+		OutstandingShares: 611000000,
+		Employees: []*model.Employee{
+			{ID: "1", Name: "Evan", Updated: false},
+		},
+		Founded:      1939,
+		Headquarters: "Gotham City",
+		Industry:     "Conglomerate",
+	},
+	{
+		ID:                "5",
+		Name:              "Winbond Electronics Crop.",
+		StockPrice:        1011.35,
+		OutstandingShares: 973000000,
+		Employees: []*model.Employee{
+			{ID: "1", Name: "Mika", Updated: false},
+		},
+		Founded:      2002,
+		Headquarters: "Boston, MA",
+		Industry:     "Science & Technology",
+	},
+	{
+		ID:                "6",
+		Name:              "Galaxy Software Services",
+		StockPrice:        229.97,
+		OutstandingShares: 2496000000,
+		Employees: []*model.Employee{
+			{ID: "1", Name: "Luka", Updated: false},
+		},
+		Founded:      1964,
+		Headquarters: "Buckinghamshire, England",
+		Industry:     "Food & Beverage",
+	},
+	{
+		ID:                "7",
+		Name:              "Vanshi Global",
+		StockPrice:        208.91,
+		OutstandingShares: 2813000000,
+		Employees: []*model.Employee{
+			{ID: "1", Name: "Iris", Updated: false},
+		},
+		Founded:      1997,
+		Headquarters: "Los Angeles, CA",
+		Industry:     "Robotics",
+	},
+}
