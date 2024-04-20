1298b61ce05cf81a62b9c9d15adbbcc12504552d
Edward <edward821220@gmail.com>
2024-04-20 13:05:39 +0800

    add readme and todo

--- client/README.md
+++ client/README.md
@@ -4,8 +4,12 @@
 
 - Step1: Run `bun install` or `npm install`
 
-- Step2: Run `watch` script in package.json to generate the types files for graphql api.
+- Step2: Update the API URL.
+
+- Step3: Run `watch` script in package.json to generate the types files for graphql api.
 
 #### Todo (Basic)
+
+- See the comments in src/App.tsx file
 
 #### Todo (Advanced)
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,15 @@
-import { useMutation, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { gql } from "./__generated__/gql";
 
-const GET_COMPANY = gql(
-  `query GetCompany($id: ID!){
-    company(id: $id) {
-      id
-      name
-      stock_price
-      outstanding_shares
-      employees {
-        id
-        name
-        updated
-      }
-    }
-  }
-`
-);
+// TODO 1 - Define GET_COMPANY gql
+const GET_COMPANY = gql(`query`);
 
-const UPDATE_EMPLOYEE = gql(`
-  mutation UpdateEmployee($companyId: ID!, $employeeId: ID!) {
-    updateEmployee(companyId: $companyId, employeeId: $employeeId) {
-      id
-      name
-      updated
-    }
-  }
-`);
+// TODO 4 - Define UPDATE_EMPLOYEE gql
 
 function App() {
-  const { loading, error, data } = useQuery(GET_COMPANY, {
-    variables: { id: "2" },
-  });
-  const [updateEmployee] = useMutation(UPDATE_EMPLOYEE, {
-    variables: { companyId: "2", employeeId: "2" },
-  });
+  // TODO 2 - use GET_COMPANY gql to fetch data
+  const { loading, error, data } = useQuery();
+  // TODO 5 - use UPDATE_EMPLOYEE gql to update data and set the fn to be called when button is clicked, render the updated status in the UI
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
@@ -42,26 +17,19 @@ function App() {
   return (
     <main className="max-w-6xl mx-auto py-6">
       <h1 className="text-center text-6xl font-bold">Companies List</h1>
+      {/* TODO 3 - Render data */}
       <ul>
-        <li>公司名稱：{data?.company?.name}</li>
-        <li>股價：{data?.company?.stock_price}</li>
-        <li>
-          市值：
-          {((data?.company?.outstanding_shares || NaN) / 1000) *
-            (data?.company?.stock_price || NaN)}
-        </li>
-        <li>員工人數：{data?.company?.employees?.length}</li>
+        <li>公司名稱：</li>
+        <li>股價：</li>
+        <li>市值：</li>
+        <li>員工人數：</li>
         <li>
           員工列表：
-          {data?.company?.employees?.map((employee) => (
-            <div key={employee.id}>
-              姓名：{employee.name} (已更新：{employee.updated ? "是" : "否"})
-            </div>
-          ))}
+          <div>姓名：(已更新：)</div>
         </li>
       </ul>
       <button
-        onClick={() => updateEmployee()}
+        onClick={() => {}}
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-8"
       >
         Update
